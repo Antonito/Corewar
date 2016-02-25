66e850e746fabd3fc682ce2bcca41f50c0659630
Antoine Baché <antoine.bache@epitech.eu>
2016-02-25 02:37:43 +0100

    Airmax and pdd champions decompilation OK

--- src/decompiler/case/affCase.c
+++ src/decompiler/case/affCase.c
@@ -5,25 +5,41 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Thu Feb 25 01:03:06 2016 Antoine Baché
-** Last update Thu Feb 25 01:35:07 2016 Antoine Baché
+** Last update Thu Feb 25 02:35:58 2016 Antoine Baché
 */
 
 #include <unistd.h>
 #include "decompiler.h"
 #include "tools.h"
 
-int	affCase(t_read *data)
+int	affRegister(t_read *data)
 {
-  int	tmp;
+  unsigned char	str[3];
 
   my_bzero(data->buff, 5);
-  if (read(data->fd, data->buff, 4) < 0)
+  if (read(data->fd, data->buff, 2) < 0)
     return (1);
-  tmp = data->buff[0] * 256 * 256 * 256 +
-    data->buff[1] * 256 * 256 + data->buff[2] * 256 + data->buff[3];
-  if (write(data->new, "%", 1) < 0 ||
-      my_write_nb(tmp, 1, data->new) < 0 ||
-      write(data->new, "\n\t", 2) < 0)
+  if (data->buff[0] != 64 || data->buff[1] < 1 || data->buff[1] > 16)
+    return (1);
+  my_bzero(&str, 3);
+  if (data->buff[1] <= 9)
+    str[0] = data->buff[1] + '0';
+  else
+    {
+      str[0] = '1';
+      str[1] = (data->buff[1] - 10) + '0';
+    }
+  if (write(data->new, "r", 1) < 0 ||
+      write(data->new, str, my_strlen(str)) < 0)
+    return (1);
+  return (0);
+}
+
+int	affCase(t_read *data)
+{
+  my_bzero(data->buff, 5);
+  if (write(data->new, data->inst[15], my_strlen(data->inst[15])) < 0 ||
+      affRegister(data))
     return (1);
   return (0);
 }
--- src/decompiler/case/stCase.c
+++ src/decompiler/case/stCase.c
@@ -5,14 +5,27 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Thu Feb 25 01:03:06 2016 Antoine Baché
-** Last update Thu Feb 25 01:36:17 2016 Antoine Baché
+** Last update Thu Feb 25 02:30:24 2016 Antoine Baché
 */
 
 #include <unistd.h>
 #include "decompiler.h"
 #include "tools.h"
 
-int	stCase(t_read *data)
+int	stIndirect(t_read *data)
+{
+  int	tmp;
+
+  my_bzero(data->buff, 5);
+  if (read(data->fd, data->buff, 2) < 0)
+    return (1);
+  tmp = data->buff[0] * 256 + data->buff[1];
+  if (my_write_nb(tmp, 1, data->new) < 0)
+    return (1);
+  return (0);
+}
+
+int	stDirect(t_read *data)
 {
   int	tmp;
 
@@ -22,8 +35,58 @@ int	stCase(t_read *data)
   tmp = data->buff[0] * 256 * 256 * 256 +
     data->buff[1] * 256 * 256 + data->buff[2] * 256 + data->buff[3];
   if (write(data->new, "%", 1) < 0 ||
-      my_write_nb(tmp, 1, data->new) < 0 ||
-      write(data->new, "\n\t", 2) < 0)
+      my_write_nb(tmp, 1, data->new) < 0)
     return (1);
+  return (0);
+}
+
+int	stRegister(t_read *data)
+{
+  unsigned char	str[3];
+
+  my_bzero(data->buff, 5);
+  if (read(data->fd, data->buff, 1) < 0)
+    return (1);
+  if (data->buff[0] < 1 || data->buff[0] > 16)
+    return (1);
+  my_bzero(&str, 3);
+  if (data->buff[0] <= 9)
+    str[0] = data->buff[0] + '0';
+  else
+    {
+      str[0] = '1';
+      str[1] = (data->buff[0] - 10) + '0';
+    }
+  if (write(data->new, "r", 1) < 0 ||
+      write(data->new, str, my_strlen(str)) < 0)
+    return (1);
+  return (0);
+}
+
+int	stCase(t_read *data)
+{
+  int	code;
+  int	i;
+
+  my_bzero(data->buff, 5);
+  if (read(data->fd, data->buff, 1) < 0 ||
+      write(data->new, data->inst[2], my_strlen(data->inst[2])) < 0)
+    return (1);
+  data->byte.bytecode = data->buff[0];
+  i = -1;
+  while (++i < 4)
+    {
+      code = getByteCode(&data->byte);
+      my_bzero(data->buff, 5);
+      if (!code && stRegister(data))
+	return (1);
+      if (code == 1 && stDirect(data))
+	return (1);
+      if (code == 2 && stIndirect(data))
+	return (1);
+      if (code != -1 && i != 1 && write(data->new, ", ", 2) < 0)
+	return (1);
+      data->byte.bytecode <<= 2;
+    }
   return (0);
 }

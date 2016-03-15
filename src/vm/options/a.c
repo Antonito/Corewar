d31aabad9c58089f5004ee283fcdd56b036224e8
Antoine Baché <antoine.bache@epitech.eu>
2016-03-15 08:54:26 +0100

    Fixed -a (added modulo MEM_SIZE)

--- src/vm/options/a.c
+++ src/vm/options/a.c
@@ -5,7 +5,7 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Fri Mar 11 05:08:44 2016 Antoine Baché
-** Last update Sat Mar 12 05:44:26 2016 Antoine Baché
+** Last update Tue Mar 15 08:53:43 2016 Antoine Baché
 */
 
 #include <unistd.h>
@@ -17,9 +17,9 @@ int	aOption(t_hero *heros, int i, char **av)
 {
   if (!(av[i]) || !isNum(av[i]))
     return (write(2, "Invalid load_address\n", 21), 1);
-  if ((heros->loadAddress = my_getnbr(av[i])) < 0 ||
-      heros->loadAddress > MEM_SIZE)
+  if ((heros->loadAddress = my_getnbr(av[i])) < 0)
     return (write(2, "Invalid load_address\n", 21), 1);
+  heros->loadAddress %= MEM_SIZE;
   heros->customAddress = true;
   return (0);
 }

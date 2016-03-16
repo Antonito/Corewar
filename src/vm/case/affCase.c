b77a202b924d2b77b6cc4c76a1f736223e79247b
Antoine Baché <antoine.bache@epitech.eu>
2016-03-16 22:00:52 +0100

    Added affCase

--- src/vm/case/affCase.c
+++ src/vm/case/affCase.c
@@ -5,7 +5,7 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Wed Mar 16 14:16:27 2016 Antoine Baché
-** Last update Wed Mar 16 16:46:40 2016 Antoine Baché
+** Last update Wed Mar 16 22:00:34 2016 Antoine Baché
 */
 
 #include "corewar.h"
@@ -13,6 +13,13 @@
 int	affCase(t_hero *hero, t_instruct *new, unsigned char *map,
 		int endianness)
 {
+  int		check;
+  t_bytecode	byte;
+
+  byte.bytecode = (char)map[hero->loadAddress + hero->pc++];
+  if (!(check = getByteCode(&byte)))
+    new->args[0] =
+      hero->reg[((map[hero->loadAddress + hero->pc++] - 1) % REG_SIZE)] % 256;
   new->time = AFF_TIME;
   return (0);
 }
--- src/vm/case/andCase.c
+++ src/vm/case/andCase.c
@@ -5,7 +5,7 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Wed Mar 16 14:16:27 2016 Antoine Baché
-** Last update Wed Mar 16 21:49:53 2016 Antoine Baché
+** Last update Wed Mar 16 21:58:48 2016 Antoine Baché
 */
 
 #include "corewar.h"
@@ -24,7 +24,8 @@ int		andCase(t_hero *hero, t_instruct *new, unsigned char *map,
     {
       if (!(check = getByteCode(&byte)))
 	new->args[i] =
-	  hero->reg[(map[hero->loadAddress + hero->pc++] % REG_SIZE)] % IDX_MOD;
+	  hero->reg[((map[hero->loadAddress + hero->pc++] -1) % REG_SIZE)]
+	  % IDX_MOD;
       else if (check == 1 && i < 2)
 	new->args[i] = readInt(hero, map, endianness) % IDX_MOD;
       else if (check == 2 && i < 2)
